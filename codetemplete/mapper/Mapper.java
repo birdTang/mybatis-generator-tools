373668ae124ea63f50d4136da9c0144233b1aadf
tangpeng <tangpeng@0easy.com>
2018-07-04 08:58:33 +0800

    修改模板

--- codetemplete/mapper/Mapper.java
+++ codetemplete/mapper/Mapper.java
@@ -10,4 +10,8 @@ import com.yihao01.campus.client.base.mapper.BaseMapper;
  */
 public interface ##{domainObjectName}##Mapper extends BaseMapper<##{domainObjectName}##,##{tablePrimaryKeyType}##>{
 	
+	/*****************center 接口使用*********************/
+	
+	/*****************cloud 后台使用*********************/
+	
 }
